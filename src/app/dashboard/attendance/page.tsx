2dfc777dfbc2f1640b33f885fa21d7d88b9993c6
soyuz.alpha <saepudinasep86@gmail.com>
2025-12-05 14:24:18 +0700

    update

--- src/app/dashboard/attendance/page.tsx
+++ src/app/dashboard/attendance/page.tsx
@@ -11,10 +11,19 @@ export default function Attendance() {
     <AppContainer title="Attendance">
       <div className="flex flex-col gap-6">
         <Tabs defaultValue="period">
-          <TabsList className="min-w-full overflow-x-scroll">
-            <TabsTrigger value="period">Period</TabsTrigger>
-            <TabsTrigger value="summary">Summary</TabsTrigger>
-          </TabsList>
+          <div
+            className="w-full overflow-x-auto scrollbar-hide"
+            style={{
+              msOverflowStyle: "none",
+              scrollbarWidth: "none",
+              WebkitOverflowScrolling: "touch",
+            }}
+          >
+            <TabsList className="min-w-full overflow-x-scroll">
+              <TabsTrigger value="period">Period</TabsTrigger>
+              <TabsTrigger value="summary">Summary</TabsTrigger>
+            </TabsList>
+          </div>
 
           <TabsContent value="period">
             <Suspense fallback={<div>Loading Period...</div>}>
